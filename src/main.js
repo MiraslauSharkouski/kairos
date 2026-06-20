af5e6c3e0c9ad451f330f64bffc5d6300a3c0eca
Miraslau <miraslausharkouski@gmail.com>
2026-06-20 23:13:36 +0300

    add logo

--- src/main.js
+++ src/main.js
@@ -2,32 +2,5 @@ import "./assets/css/style.css";
 import viteLogo from "./assets/vite.svg";
 
 document.querySelector("#app").innerHTML = `
-<header class="header">
-  <div class="logo">
 
-  </div>
-   <div class="header_links">
-      <nav class="links">
-        <ul>
-          <li><a href="#!">home</a></li>
-        </ul>
-        <ul>
-          <li><a href="#!">about us</a></li>
-        </ul>
-        <ul>
-          <li><a href="#!">projects</a></li>
-        </ul>
-        <ul>
-          <li><a href="#!">contact us</a></li>
-        </ul>
-      </nav>
-  </div>
-</header>  
-<section id="hero">
-  hero
-</section>
-
-<section id="online_banking">
-  online bancking
-</section>
 `;
