5045788b0ba9794a94bf98635030a8bddb8c3395
Miraslau <miraslausharkouski@gmail.com>
2026-06-21 10:32:34 +0300

    completed header and it is adaptive

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
-import "./assets/css/style.css";
-import viteLogo from "./assets/vite.svg";
+const burger = document.getElementById("burger");
+const headerLinks = document.querySelector(".header_links");
 
-document.querySelector("#app").innerHTML = `
+burger?.addEventListener("click", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  burger.classList.toggle("is-active");
+  headerLinks?.classList.toggle("is-active");
+  document.body.classList.toggle("no-scroll");
+});
 
-`;
+document.querySelectorAll(".header_links nav a").forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    burger?.classList.remove("is-active");
+    headerLinks?.classList.remove("is-active");
+    document.body.classList.remove("no-scroll");
+  });
+});
